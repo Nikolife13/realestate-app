42a4a38b84002e979ff47aab874066dd5e5af505
Nikolife13 <nikolife13@gmail.com>
2026-04-11 22:57:26 +0100

    chore: dynamic db config and enable ddl-auto update

--- target/classes/static/auth.js
+++ target/classes/static/auth.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8080/api';
+const API_BASE = '/api';
 
 // Login handler
 const loginForm = document.getElementById('loginForm');
--- target/classes/static/detail.js
+++ target/classes/static/detail.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:8080/api';
+const API = '/api';
 let currentImages = [];
 let currentIndex = 0;
 let carouselContainer = null;
